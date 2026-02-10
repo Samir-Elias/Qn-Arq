13d32d4b848af7da8dd02ec016e3f7dbd58e6cc6
Samir Elias <samireliassalatino@gmail.com>
2026-02-10 01:10:42 -0300

    fix: add dynamic export to enable forced dynamic rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { getProjects, getFeaturedProject } from "@/lib/data/projects";
 import { HomeContent } from "@/components/HomeContent";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
   const [featured, projects] = await Promise.all([
     getFeaturedProject(),
